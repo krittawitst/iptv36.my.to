9ec1182585f25f5fbd8a0cb84698547d6b0a3171
Wit Krittawit <krittawit.st@gmail.com>
2020-10-11 12:45:46 +0700

    added new epg, disable gh action

--- src/builder.js
+++ src/builder.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
-const axios = require("axios");
-const format = require("date-format");
+const fs = require('fs');
+const getStreamingInfo = require('./streaming.js');
+const getEpgData = require('./epg.js');
+const allPlaylist = require('./playlist.js');
 
-const getStreamingInfo = require("./streaming.js");
-const getEpgData = require("./epg.js");
-const allPlaylist = require("./playlist.js");
+const currentDatetimePlus7Hrs = new Date(new Date().getTime() + 7 * 60 * 60 * 1000);
+const currentBkkDatetimeStr = currentDatetimePlus7Hrs.toISOString().slice(0, 16);
 
 const main = async () => {
   // M3U PLAYLIST
   for (let playlist of allPlaylist) {
-    let textStr = `#EXTM3U : Thai IPTV Playlist from https://iptv36.my.to/ - Last Update ${format()}\n\n`;
+    let textStr = `#EXTM3U : Thai IPTV Playlist from https://iptv36.my.to/ - Last Update ${currentBkkDatetimeStr}\n\n`;
 
     for (let i = 0; i < playlist.channelList.length; i++) {
       let [channelKey, skip = 0] = playlist.channelList[i];
@@ -18,13 +18,11 @@ const main = async () => {
         streamingInfo.tvgId
       }" group-title="${streamingInfo.groupName}" tvg-logo-small="${
         streamingInfo.logo
-      }" tvg-logo="${streamingInfo.logo}", ${streamingInfo.channelName}\n${
-        streamingInfo.url
-      }\n\n`;
+      }" tvg-logo="${streamingInfo.logo}", ${streamingInfo.channelName}\n${streamingInfo.url}\n\n`;
       textStr = textStr + `${channelStr}`;
     }
 
-    fs.writeFileSync(`${playlist.filename}`, textStr, "utf8");
+    fs.writeFileSync(`${playlist.filename}`, textStr, 'utf8');
 
     console.log(`==> Created playlist '${playlist.filename}'\n`);
   }
@@ -36,11 +34,11 @@ const main = async () => {
 <!DOCTYPE tv SYSTEM "xmltv.dtd">
 <tv>
 `;
-  let xmlTail = "</tv>";
+  let xmlTail = '</tv>';
 
   let allTvgTagId = [];
 
-  let xmlProgramBody = "";
+  let xmlProgramBody = '';
   for (let epg of epgData) {
     xmlProgramBody += `  <programme start="${epg.programStartStr}" stop="${epg.programEndStr}" channel="${epg.tvgTagId}">\n`;
     xmlProgramBody += `    <title><![CDATA[${epg.programTitle}]]></title>\n`;
@@ -52,7 +50,7 @@ const main = async () => {
     allTvgTagId.push(epg.tvgTagId);
   }
 
-  let xmlChannelBody = "";
+  let xmlChannelBody = '';
   for (let tvgTagId of new Set(allTvgTagId)) {
     xmlChannelBody += `  <channel id="${tvgTagId}">
     <display-name>${tvgTagId}</display-name>
@@ -60,11 +58,7 @@ const main = async () => {
 `;
   }
 
-  fs.writeFileSync(
-    "EPG.xml",
-    xmlHead + xmlChannelBody + xmlProgramBody + xmlTail,
-    "utf8"
-  );
+  fs.writeFileSync('EPG.xml', xmlHead + xmlChannelBody + xmlProgramBody + xmlTail, 'utf8');
   console.log(`==> Created EPG 'EPG.xml'`);
 };
 
--- src/epg.js
+++ src/epg.js
@@ -1,13 +1,11 @@
-const fs = require("fs");
-const axios = require("axios");
-const format = require("date-format");
+const axios = require('axios');
 
 const getEpgDataFromNbtc = async () => {
   // send request
   let rawData = {};
   try {
     const epgUrl =
-      "http://dtvguide.nbtc.go.th/NbtcMobileWebService/nbtc.mobile.service/epgservice/getAllChannelTemp";
+      'http://dtvguide.nbtc.go.th/NbtcMobileWebService/nbtc.mobile.service/epgservice/getAllChannelTemp';
     const response = await axios.post(epgUrl);
     rawData = response.data;
   } catch (error) {
@@ -17,9 +15,7 @@ const getEpgDataFromNbtc = async () => {
 
   // get program start now + 2 days
   let currentDatetime = new Date();
-  let currentDatetimePlusTwoDays = new Date(
-    currentDatetime.getTime() + 86400 * 2 * 1000
-  );
+  let currentDatetimePlus2Days = new Date(currentDatetime.getTime() + 86400 * 2 * 1000);
 
   // format raw data
   let epgData = [];
@@ -28,23 +24,18 @@ const getEpgDataFromNbtc = async () => {
     for (let program of result.programOfChannel) {
       let programStart = new Date(program.pgBeginTimeLong * 1000);
       let programEnd = new Date(program.pgEndTimeLong * 1000);
-      if (
-        programEnd < currentDatetime ||
-        programStart > currentDatetimePlusTwoDays
-      ) {
+      if (programEnd < currentDatetime || programStart > currentDatetimePlus2Days) {
         continue;
       }
       let programStartStr = `${programStart
         .toISOString()
-        .replace(/-|:|T/g, "")
-        .replace(".000Z", "")} -0000`;
+        .replace(/-|:|T/g, '')
+        .replace('.000Z', '')} -0000`;
       let programEndStr = `${programEnd
         .toISOString()
-        .replace(/-|:|T/g, "")
-        .replace(".000Z", "")} -0000`;
-      let programTitle = program.pgTitle
-        ? program.pgTitle.trim()
-        : "No Program Name";
+        .replace(/-|:|T/g, '')
+        .replace('.000Z', '')} -0000`;
+      let programTitle = program.pgTitle ? program.pgTitle.trim() : 'No Program Name';
       let programDescription = undefined;
       if (program.pgDesc && program.pgDesc.trim()) {
         programDescription = program.pgDesc.trim();
@@ -64,24 +55,26 @@ const getEpgDataFromNbtc = async () => {
 const getEpgDataFromAis = async () => {
   // mapping tvg id
   let channelIdToTvgId = {
-    "5efdd162fbb0045345ef2b61": "th-dtv4.iptv36.my.to",
-    "597e004b7ed5a24e46f6725a": "warnertv.iptv36.my.to",
-    "5e44faeeaae73158d325f8f9": "hitsmovies.iptv36.my.to",
+    '5efdd162fbb0045345ef2b61': 'th-dtv4.iptv36.my.to',
+    '597e004b7ed5a24e46f6725a': 'warnertv.iptv36.my.to',
+    '5e44faeeaae73158d325f8f9': 'hitsmovies.iptv36.my.to',
+    '596716e6bf6aee05dcdfc119': 'voice.iptv36.my.to',
+    '5e69984f609ced33cfa4e734': 'bein1.iptv36.my.to',
+    '5e699a69bf6aee30a499dc9b': 'bein2.iptv36.my.to',
+    '5a52f069aae731507f5387bb': 'cartoonnetwork.iptv36.my.to',
+    '5ee1ecafb544d498b9d1d2e8': 'nickelodeon.iptv36.my.to',
+    '597d21477ed5a24e46f67258': 'cartoonclub.iptv36.my.to',
+    '5e6215e6d817de33506cedf7': 'discoveryasia.iptv36.my.to',
   };
 
   // build parameter
   let currentDatetime = new Date();
-  let currentBkkDatetime = new Date(
-    currentDatetime.getTime() + 7 * 60 * 60 * 1000
-  );
-  let currentBkkDatetimePlusTwoDays = new Date(
-    currentBkkDatetime.getTime() + 86400 * 2 * 1000
-  );
-  let startBkkDateStr = currentBkkDatetime.toISOString().slice(0, 10);
-  let startBkkTimeStr = currentBkkDatetime.toISOString().slice(11, 16) + ":00";
-  let endBkkDateStr = currentBkkDatetimePlusTwoDays.toISOString().slice(0, 10);
-  let endBkkTimeStr =
-    currentBkkDatetimePlusTwoDays.toISOString().slice(11, 16) + ":00";
+  let currentDatetimePlus7Hrs = new Date(currentDatetime.getTime() + 7 * 60 * 60 * 1000);
+  let currentDatetimePlus2Days7Hrs = new Date(currentDatetimePlus7Hrs.getTime() + 86400 * 2 * 1000);
+  let startBkkDateStr = currentDatetimePlus7Hrs.toISOString().slice(0, 10);
+  let startBkkTimeStr = currentDatetimePlus7Hrs.toISOString().slice(11, 16) + ':00';
+  let endBkkDateStr = currentDatetimePlus2Days7Hrs.toISOString().slice(0, 10);
+  let endBkkTimeStr = currentDatetimePlus2Days7Hrs.toISOString().slice(11, 16) + ':00';
 
   // send request
   let rawData = {};
@@ -98,11 +91,9 @@ const getEpgDataFromAis = async () => {
   let epgData = [];
   for (let item of rawData.items) {
     let tvgTagId = channelIdToTvgId[item.parent];
-    let programStartStr =
-      `${item.date}${item.start}`.replace(/-|:|T/g, "") + " +0700";
-    let programEndStr =
-      `${item.date_end}${item.end}`.replace(/-|:|T/g, "") + " +0700";
-    let programTitle = item.title ? item.title.trim() : "No Program Name";
+    let programStartStr = `${item.date}${item.start}`.replace(/-|:|T/g, '') + ' +0700';
+    let programEndStr = `${item.date_end}${item.end}`.replace(/-|:|T/g, '') + ' +0700';
+    let programTitle = item.title ? item.title.trim() : 'No Program Name';
     let programDescription = undefined;
     if (item.synopsis && item.synopsis.trim() && item.synopsis !== item.title) {
       programDescription = item.synopsis.trim();
