0a94f212e35f40d43457932baa35b4e9ebec37e7
Wit Krittawit <47003818+krittawitst@users.noreply.github.com>
2020-11-16 13:58:09 +0700

    Add files via upload

--- functions/true.js
+++ functions/true.js
@@ -1,1 +1,14 @@
-init
+exports.handler = async (event, context, callback) => {
+  console.log(event);
+  console.log('='.repeat(30));
+  console.log(context);
+  console.log('='.repeat(30));
+  console.log(callback);
+  return {
+    statusCode: 302,
+    headers: {
+      location: 'https://www.google.co.th',
+    },
+    body: '',
+  };
+};
