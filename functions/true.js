f7517532312303d2fc2fb2198a75237308c0e3be
Wit Krittawit <47003818+krittawitst@users.noreply.github.com>
2020-11-21 18:44:53 +0700

    Add files via upload

--- functions/true.js
+++ functions/true.js
@@ -7,13 +7,12 @@ exports.handler = async (event, context, callback) => {
     true4u: `https://true4u.com/live-api?ip=127.0.0.1&uid=${Math.random()
       .toString(36)
       .substring(2, 12)}&session=${Math.random().toString(36).substring(2, 12)}`,
-    fwmov: 'https://www.fwiptv.cc/player_demo.php?channel=89782',
   };
 
   let channel = event.queryStringParameters.channel;
 
   if (!Object.keys(config).includes(channel)) {
-    return { statusCode: 404, body: 'Parameter "channel" is required.' };
+    return { statusCode: 404, body: 'Parameter "channel" is required and it must be valid value.' };
   }
 
   let pageUrl = config[channel];
@@ -27,9 +26,11 @@ exports.handler = async (event, context, callback) => {
     return { statusCode: 404, body: `Cannot get data for channel "${channelKey}" at "${pageUrl}"` };
   }
 
-  let regExpMatchArray = rawData.match(/https?:\/\/.+?\.m3u8(\?[^"']+)?/);
+  let regExpMatchArray = rawData.match(/https:\/\/.[^"']+?\.m3u8(\?[^"']+)?/);
   if (regExpMatchArray) {
-    streamingUrl = regExpMatchArray[0].replace('m_auto_tidl', 'w_auto_tidapp');
+    streamingUrl = regExpMatchArray[0]
+      .replace('%3D%3D', '')
+      .replace('m_auto_tidl', 'w_auto_tidapp');
   }
 
   console.log(`request ${event.queryStringParameters.channel}\nreturn ${streamingUrl}`);
