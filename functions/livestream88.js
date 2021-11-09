922e13296bd5fd8b4c1857824bdaa7284400312a
Wit Krittawit <47003818+krittawitst@users.noreply.github.com>
2021-11-09 16:31:06 +0700

    Add files via upload

--- functions/livestream88.js
+++ functions/livestream88.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 
 exports.handler = async (event, context, callback) => {
+  const streamingUrl =
+    'https://nj2yx-gbi9-cdf5.googlecdncontent.com/livestream88_sport/tpf1/playlist.m3u8';
   return {
     statusCode: 302,
     headers: {
-      location:
-        'https://nj2yx-gbi9-cdf5.googlecdncontent.com/livestream88_sport/tpf1/playlist.m3u8',
+      location: streamingUrl,
       Referer: 'https://www.livestream88.com/',
     },
     body: `Go to ${streamingUrl}`,
