bcbdbe390a1d89f8dc505ef602dcbe07b9c15a9b
Wit Krittawit <47003818+krittawitst@users.noreply.github.com>
2021-11-09 17:26:15 +0700

    Add files via upload

--- functions/livestream88.js
+++ functions/livestream88.js
@@ -3,12 +3,19 @@ const axios = require('axios');
 exports.handler = async (event, context, callback) => {
   const streamingUrl =
     'https://nj2yx-gbi9-cdf5.googlecdncontent.com/livestream88_sport/tpf1/playlist.m3u8';
+  let body = '';
+
+  try {
+    const response = await axios.get(pageUrl, {
+      headers: { Referer: 'https://www.livestream88.com/' },
+    });
+    body = response.data;
+  } catch (error) {
+    return { statusCode: 404, body: `Cannot get data for channel "${channelKey}" at "${pageUrl}"` };
+  }
+
   return {
-    statusCode: 302,
-    headers: {
-      location: streamingUrl,
-      Referer: 'https://www.livestream88.com/',
-    },
-    body: `Go to ${streamingUrl}`,
+    statusCode: 200,
+    body: body,
   };
 };
