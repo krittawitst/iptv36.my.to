1733c1f35ec2f0f8025294ba3aa157d733b06dfb
Wit Krittawit <47003818+krittawitst@users.noreply.github.com>
2023-03-09 11:26:53 +0700

    Add files via upload

--- functions/true.js
+++ functions/true.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 exports.handler = async (event, context, callback) => {
   // config
   let config = {
-    tnn16hd: `https://www.tnnthailand.com/live-api/?ip=127.0.0.1&uid=${Math.random()
-      .toString(36)
-      .substring(2, 12)}&session=${Math.random().toString(36).substring(2, 12)}`,
+    // tnn16hd: `https://www.tnnthailand.com/live-api/?ip=127.0.0.1&uid=${Math.random()
+    //   .toString(36)
+    //   .substring(2, 12)}&session=${Math.random().toString(36).substring(2, 12)}`,
+    tnn16hd: `https://www.tnnthailand.com/content-public-api/signer-url?prefix=/live/`,
     true4uhd: `https://true4u.com/live-api/signer-url?prefix=/live`,
   };
 
@@ -24,12 +25,13 @@ exports.handler = async (event, context, callback) => {
     rawData = response.data;
 
     if (channel === 'tnn16hd') {
-      let regExpMatchArray = rawData.match(/https:\/\/.[^"']+?\.m3u8(\?[^"']+)?/);
-      if (regExpMatchArray) {
-        streamingUrl = regExpMatchArray[0]
-          .replace('%3D%3D', '')
-          .replace('m_auto_tidl', 'w_auto_tidapp');
-      }
+      // let regExpMatchArray = rawData.match(/https:\/\/.[^"']+?\.m3u8(\?[^"']+)?/);
+      // if (regExpMatchArray) {
+      //   streamingUrl = regExpMatchArray[0]
+      //     .replace('%3D%3D', '')
+      //     .replace('m_auto_tidl', 'w_auto_tidapp');
+      // }
+      streamingUrl = rawData;
     } else if (channel === 'true4uhd') {
       streamingUrl = rawData.replace('playlist.m3u8', 'pl_720p/index.m3u8');
     }
@@ -37,11 +39,6 @@ exports.handler = async (event, context, callback) => {
     console.log(`request ${event.queryStringParameters.channel}\nreturn ${streamingUrl}`);
     console.log('='.repeat(20));
   } catch (error) {
-    if (channel === 'tnn16hd') {
-      streamingUrl = 'http://freelive2.inwstream.com:1935/freelive-edge/tnn24/playlist.m3u8';
-    } else if (channel === 'true4uhd') {
-      streamingUrl = 'http://freelive.inwstream.com:1935/freelive-edge/true4u/playlist.m3u8';
-    }
     console.error(`request ${event.queryStringParameters.channel}\nreturn ${error}`);
     console.error('='.repeat(20));
   }
